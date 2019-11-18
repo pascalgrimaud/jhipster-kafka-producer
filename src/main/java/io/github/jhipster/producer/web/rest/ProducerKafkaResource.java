e4bda0d44f05fb96470b7710948fe820adf49436
Fanny Daudies <fannydaudies@hotmail.fr>
2019-11-18 15:21:51 +0100

    Add new endpoint to send json message to kafka topic

--- src/main/java/io/github/jhipster/producer/web/rest/ProducerKafkaResource.java
+++ src/main/java/io/github/jhipster/producer/web/rest/ProducerKafkaResource.java
@@ -1,12 +1,12 @@
 package io.github.jhipster.producer.web.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.jhipster.producer.service.ProducerKafkaProducer;
+import io.github.jhipster.producer.service.JsonKafkaProducer;
+import io.github.jhipster.producer.web.rest.vm.FooVM;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/producer-kafka")
@@ -16,13 +16,23 @@ public class ProducerKafkaResource {
 
     private ProducerKafkaProducer kafkaProducer;
 
-    public ProducerKafkaResource(ProducerKafkaProducer kafkaProducer) {
+    private JsonKafkaProducer jsonProducer;
+
+    public ProducerKafkaResource(ProducerKafkaProducer kafkaProducer, JsonKafkaProducer jsonProducer) {
         this.kafkaProducer = kafkaProducer;
+        this.jsonProducer = jsonProducer;
     }
 
     @PostMapping("/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
         log.debug("REST request to send to Kafka topic the message : {}", message);
         this.kafkaProducer.send(message);
+    }
+
+    @PostMapping("/publish-json")
+    public void sendJsonMessageToKafkaTopic(@RequestBody FooVM foo) {
+        log.debug("REST request to send to Kafka topic the json message : {}", foo);
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.jsonProducer.send(objectMapper.valueToTree(foo));
     }
 }
---
+++ src/main/java/io/github/jhipster/producer/web/rest/vm/FooVM.java
@@ -0,0 +1,43 @@
+package io.github.jhipster.producer.web.rest.vm;
+
+public class FooVM {
+
+    String field1;
+
+    String field2;
+
+    int number;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2 = field2;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "FooVM[" +
+            "field1='" + field1 + '\'' +
+            ", field2='" + field2 + '\'' +
+            ", number=" + number +
+            ']';
+    }
+}
